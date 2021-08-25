1b141f8c61bc7fd2c5db982f932b9c5f3bc684b7
MAngelFM <88851006+MAngelFM@users.noreply.github.com>
2021-08-25 00:52:08 -0500

    Update exercise.py

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,5 +1,22 @@
-def main():
-    #escribe tu código abajo de esta línea
-    pass
-if __name__=='__main__':
-    main()
+#The BMI IF ELSE
+print("Give me a Weight ")
+Weight=float(input())
+print("Give me a Height ")
+Height=float(input())
+
+if Weight == 0:
+    print("Revisa tus datos, alguno de ellos es erroneo")
+elif Height == 0:
+    print("Revisa tus datos, alguno de ellos es erroneo")
+bmi = Weight / (Height ** 2)
+
+if 0 < bmi < 20 :
+    print("PESO BAJO")
+elif 20 <= bmi < 25:
+    print("PESO NORMAL")
+elif 25 <= bmi < 30:
+    print("SOBREPESO")
+elif 30 <= bmi < 40:
+    print("OBESIDAD")
+elif bmi >= 25:
+    print("OBESIDAD MORBIDA")
