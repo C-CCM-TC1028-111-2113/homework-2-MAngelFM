252be74a8eb6b9d76f4c7379bbbabb530d3b6bea
MAngelFM <88851006+MAngelFM@users.noreply.github.com>
2021-08-25 00:43:04 -0500

    Update exercise.py

--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -4,18 +4,30 @@ def main():
     num2 = int(input("Ingresa el segundo número: "))
     num3 = int(input("Ingresa el tercer número: "))
     if num1 > num2 > num3:
-        print("",num1, "-" ,num2, "-" ,num3)
+        print("",num1)
+        print("",num2)
+        print("",num3)
     elif num2 > num1 > num3:
-        print("",num2, "-" ,num1, "-" ,num3)
+        print("",num2)
+        print("",num1)
+        print("",num3)
     elif num3 > num1 > num2:
-        print("",num3, "-" ,num1, "-" ,num2)
+        print("",num3)
+        print("",num1)
+        print("",num2)
     elif num3 > num2 > num1:
-        print("",num3, "-" ,num2, "-" ,num1)
+        print("",num3)
+        print("",num2)
+        print("",num1)
     elif num1 > num3 > num2:
-        print("",num1, "-" ,num3, "-" ,num2)
+        print("",num1)
+        print("",num3)
+        print("",num2)
     elif num2 > num3 > num1:
-        print("",num2, "-" ,num3, "-" ,num1)
-             
+        print("",num2)
+        print("",num3)
+        print("",num1)
+
 
 if __name__ == '__main__':
     main()
